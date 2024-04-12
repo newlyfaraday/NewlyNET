2d17103c35cbebe3ef7a186063d368d50a0113f0
Faraday <165588366+newlyfaraday@users.noreply.github.com>
2024-04-12 23:36:35 +0300

    Update newly.rb

--- newly.rb
+++ newly.rb
@@ -4,25 +4,38 @@ require 'net/http'
 require 'thor'
 
 class Newly < Thor
-  desc "get URL", "Performs a GET request to the specified URL"
-  option :headers, aliases: '-H', type: :array, desc: "Additional headers as 'Key: Value' pairs"
-  option :cookies, aliases: '-C', type: :array, desc: "Additional cookies as 'Cookie: Value' pairs"
+  desc "get -U URL", "Performs a GET request to the specified URL"
 
-  def get(url)
-    uri = URI(url)
+  class_option :url, aliases: '-U', type: :string, desc: "URL for perform request"
+  class_option :output_save, aliases: "-S", type: :string, desc: "File to save the output"
+  class_option :url_list, aliases: "-S", type: :string, desc: "Set multi url list path"
+  class_option :output, aliases: "-O", type: :string, desc: "Output to be printed on the screen"
+  class_option :headers, aliases: '-H', type: :array, desc: "Additional headers as 'Key: Value' pairs", required: false
+  class_option :cookies, aliases: '-C', type: :array, desc: "Additional cookies as 'Cookie: Value' pairs", required: false
+  class_option :open_timeout, type: :numeric, desc: "Additional open page timeout value", required: false
+  class_option :read_timeout, type: :numeric, desc: "Additional read page timeout value", required: false
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = (uri.scheme == 'https')
+  def get()
+    uri = URI(options[:url])
 
     request = Net::HTTP::Get.new(uri)
-    request['Cookie'] = String
+    http(uri, request)
+  end
 
-    add_headers(request, options[:headers]) if options[:headers]
-    add_cookies(request, options[:cookies]) if options[:cookies]
+  desc "post URL", "Performs a POST request to the specified URL"
+  option :post_data, type: :array, desc: "Data to send for post requets as ''"
 
-    response = http.request(request)
+  def post()
+    uri = URI(options[:url])
 
-    puts("URL: ".dark_red + "#{uri} #{response.code} #{response.message}\n\n")
+    request = Net::HTTP::Post.new(uri)
+    http(uri, request)
+  end
+
+  private
+
+  def print_response(response, code, message)
+    puts("URL: ".dark_red + "#{options[:url]} #{code} #{message}\n\n")
 
     response.each_header do |header, value|
       puts("#{header.capitalize.dark_red}: #{value.dark_blue}")
@@ -31,7 +44,21 @@ class Newly < Thor
     puts("\n" + response.body)
   end
 
-  private
+  def http(uri, request)
+    net_http = Net::HTTP.new(uri.host, uri.port)
+    net_http.use_ssl = (uri.scheme == 'https')
+
+    net_http.open_timeout = options[:open_timeout] if @options[:open_timeout]
+    net_http.read_timeout = options[:read_timeout] if @options[:read_timeout]
+
+    request['Cookie'] = String
+
+    add_headers(request, options[:headers]) if options[:headers]
+    add_cookies(request, options[:cookies]) if options[:cookies]
+
+    response = net_http.request(request)
+    print_response(response, response.code, response.message)
+  end
 
   def add_headers(request, headers)
     headers.each do |header|
@@ -43,7 +70,6 @@ class Newly < Thor
   def add_cookies(request, cookies)
       request['Cookie'] = cookies.join('; ') if cookies
   end
-
 end
 
 class String
